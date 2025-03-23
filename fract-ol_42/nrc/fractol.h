5ff4cb321d7fb22c4b605c48cbbbe3868036ecd8
Walid El Mjiyad <wel-mjiy@e2r6p7.1337.ma>
2025-03-23 01:09:23 +0000

    do the files and folders

---
+++ fract-ol_42/nrc/fractol.h
@@ -0,0 +1,15 @@
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+
+# include "../../minilibx-linux/mlx.h"
+
+#endif
